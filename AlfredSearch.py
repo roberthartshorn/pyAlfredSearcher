ef938dc6d022c8a95b0bd525928632b9bbc566af
Robert Hartshorn <roberthartshorn@midstatespring.com>
2021-02-25 12:55:00 -0500

    Add support for loose matching

--- AlfredSearch.py
+++ AlfredSearch.py
@@ -13,7 +13,7 @@ searchFolder = os.getenv('searchFolder') if mode == "Production" else sys.argv[2
 limit = os.getenv('limit') if mode == "Production" else sys.argv[3]
 
 #Mdfind is a native Mac OS X command to search a Spotlight index.
-bashCmd = "mdfind 'kMDItemFSName = " + '"' + query + '*"' + "c"  + "' -onlyin " + '"' + searchFolder + '"' + " | head -n " + limit
+bashCmd = "mdfind 'kMDItemFSName = " + '"*' + query + '*"' + "c"  + "' -onlyin " + '"' + searchFolder + '"' + " | head -n " + limit
 proc = sp.check_output(bashCmd, shell=True)
 result = proc.decode("UTF-8").splitlines()
 
